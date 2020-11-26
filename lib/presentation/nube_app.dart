7614c8719982d31b528dcbc6bf43b55fdd6e0e36
riteshakya037 <riteshakya037@gmail.com>
2020-11-26 12:00:12 +0545

    Add i18n Localization support
    Added extension vscode-flutter-i18n-json with helps support for auto
    code generation.
    
    Link: https://marketplace.visualstudio.com/items?itemName=esskar.vscode-flutter-i18n-json
    
    Added implementation engligh localization for registration process.

--- lib/presentation/nube_app.dart
+++ lib/presentation/nube_app.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import '../constants/app_constants.dart';
 import 'nube_theme.dart';
+import 'widgets/locale/locale_builder.dart';
 
 class NubeApp extends StatelessWidget {
   const NubeApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Nube MQTT",
-      theme: NubeTheme.lightTheme,
-      home: const Placeholder(),
+    return LocaleBuilder(
+      builder: (context, i18n) {
+        return MaterialApp(
+          title: AppConstants.APP_NAME,
+          theme: NubeTheme.lightTheme,
+          home: const Placeholder(),
+          localizationsDelegates: [
+            i18n,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate
+          ],
+          supportedLocales: i18n.supportedLocales,
+        );
+      },
     );
   }
 }
