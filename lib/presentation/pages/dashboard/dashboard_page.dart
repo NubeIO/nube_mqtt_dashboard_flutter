1b3884dd89f454c0e2b21e9a96c625b5fe1f4ea3
riteshakya037 <riteshakya037@gmail.com>
2020-12-14 10:16:23 +0545

    Complete home screen
    Added drawer detail responseive layout which shows a drawer+page on a
    mobile device vs a master detail on something larger.

--- lib/presentation/pages/dashboard/dashboard_page.dart
+++ lib/presentation/pages/dashboard/dashboard_page.dart
@@ -2,21 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../application/layout/layout_cubit.dart';
+import '../../../domain/layout/failures.dart';
+import '../../../generated/i18n.dart';
 import '../../../injectable/injection.dart';
+import '../../widgets/responsive/drawer_detail_layout.dart';
+import '../../widgets/responsive/snackbar.dart';
 
 class DashboardPage extends StatelessWidget {
   final cubit = getIt<LayoutCubit>();
 
   DashboardPage({Key key}) : super(key: key);
 
+  void _onFailure(BuildContext context, LayoutFailure failure) {
+    final snackbar = ResponsiveSnackbar.build(
+      context,
+      content: Text(
+        failure.when(
+            unexpected: () => I18n.of(context).failureGeneric,
+            invalidLayout: () =>
+                "Opps! The layout doesn't seem to be valid. Please contact ."),
+        style: Theme.of(context)
+            .textTheme
+            .bodyText1
+            .copyWith(color: Theme.of(context).errorColor),
+      ),
+    );
+    Scaffold.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<LayoutCubit, LayoutState>(
-      cubit: cubit,
-      listener: (context, state) {},
-      builder: (context, state) {
-        return const Placeholder();
-      },
+    return Scaffold(
+      body: BlocConsumer<LayoutCubit, LayoutState>(
+        cubit: cubit,
+        listener: (context, state) {
+          state.layoutState.maybeWhen(
+              failure: (failure) {
+                _onFailure(context, failure);
+              },
+              orElse: () {});
+        },
+        builder: (context, state) {
+          final list = state.layoutBuilder.pages;
+          return DrawerDetailLayout(
+            appBarBuilder: (context, index) {
+              return AppBar(
+                elevation: 8,
+                title: Text(list[index].name),
+              );
+            },
+            detailBuilder: (context, selectedIndex) {
+              return Container();
+            },
+            itemBuilder: (context, index, selected, onTapCallback) {
+              return ListTile(
+                selected: selected,
+                onTap: () => onTapCallback(index),
+                title: Text(list[index].name),
+              );
+            },
+            itemCount: list.size,
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/presentation/widgets/responsive/drawer_detail_layout.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'padding.dart';
+import 'screen_type_layout.dart';
+
+typedef IndexedWidgetBuilder = Widget Function(BuildContext context, int index,
+    bool selected, void Function(int index) onTapCallback);
+typedef DetailWidgetBuilder = Widget Function(
+  BuildContext context,
+  int selectedIndex,
+);
+
+PreferredSizeWidget _defaultAppbar(BuildContext context, int key) => AppBar();
+
+class DrawerDetailLayout extends StatefulWidget {
+  final Widget _header;
+  final Widget _footer;
+  final DetailWidgetBuilder _builder;
+  final int itemCount;
+  final IndexedWidgetBuilder _itemBuilder;
+  final void Function(int key) _onSelectedChange;
+  final PreferredSizeWidget Function(BuildContext context, int key)
+      _appBarBuilder;
+
+  const DrawerDetailLayout({
+    Key key,
+    Widget header,
+    Widget footer,
+    @required DetailWidgetBuilder detailBuilder,
+    @required this.itemCount,
+    @required IndexedWidgetBuilder itemBuilder,
+    Function(int index) onSelectedChange,
+    PreferredSizeWidget Function(BuildContext context, int index)
+        appBarBuilder = _defaultAppbar,
+  })  : assert(detailBuilder != null),
+        assert(itemCount != null),
+        assert(itemBuilder != null),
+        _header = header,
+        _footer = footer,
+        _builder = detailBuilder,
+        _itemBuilder = itemBuilder,
+        _onSelectedChange = onSelectedChange,
+        _appBarBuilder = appBarBuilder,
+        super(key: key);
+
+  @override
+  _DrawerDetailLayoutState createState() => _DrawerDetailLayoutState();
+}
+
+class _DrawerDetailLayoutState extends State<DrawerDetailLayout> {
+  int selectedIndex = -1;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _fixIndex();
+  }
+
+  @override
+  void didUpdateWidget(Widget oldWidget) {
+    super.didUpdateWidget(widget);
+    if (widget.itemCount > 0 && selectedIndex == -1) {
+      _fixIndex();
+    }
+  }
+
+  void _fixIndex() {
+    setState(() {
+      selectedIndex = widget.itemCount > 0 ? 0 : -1;
+    });
+  }
+
+  void _onSelected(int selectedIndex) {
+    if (this.selectedIndex == selectedIndex) return;
+    setState(() {
+      this.selectedIndex = selectedIndex;
+    });
+    if (widget._onSelectedChange != null) {
+      widget._onSelectedChange(selectedIndex);
+    }
+  }
+
+  Widget _buildDrawer(BuildContext context, bool isMobile) {
+    final theme = Theme.of(context).copyWith(
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ),
+      iconTheme: IconTheme.of(context).copyWith(
+        color: Colors.white,
+      ),
+      primaryColor: Theme.of(context).colorScheme.secondary,
+    );
+    final bottomPadding = MediaQuery.of(context).padding.bottom;
+
+    return Theme(
+      data: theme,
+      child: Column(
+        children: [
+          DrawerHeader(
+              child: widget._header ??
+                  Center(
+                      child: Text(
+                    'Nubeio',
+                    style: theme.textTheme.headline1,
+                  ))),
+          if (widget.itemCount > 0)
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.itemCount,
+                itemBuilder: (context, position) {
+                  return widget._itemBuilder(
+                    context,
+                    position,
+                    selectedIndex == position,
+                    (index) {
+                      _onSelected(index);
+                      if (isMobile) {
+                        Navigator.pop(context);
+                      }
+                    },
+                  );
+                },
+              ),
+            )
+          else
+            Expanded(child: Container()),
+          widget._footer ?? Container(),
+          SizedBox(
+            height: bottomPadding,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _builder(BuildContext context, int selectedIndex) {
+    if (widget.itemCount > 0) {
+      return widget._builder(context, selectedIndex);
+    } else {
+      return Container();
+    }
+  }
+
+  PreferredSizeWidget _appBarBuilder(BuildContext context, int selectedIndex) {
+    if (widget.itemCount > 0) {
+      return widget._appBarBuilder(context, selectedIndex);
+    } else {
+      return AppBar();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: Scaffold(
+        key: _scaffoldKey,
+        appBar: _appBarBuilder(context, selectedIndex),
+        drawer: Drawer(
+          child: Container(
+            color: Theme.of(context).colorScheme.primary,
+            child: _buildDrawer(context, true),
+          ),
+        ),
+        body: _builder(context, selectedIndex),
+      ),
+      tablet: Stack(
+        children: [
+          Row(children: [
+            SizedBox(
+              width: ResponsiveSize.MASTER_PANEL_WIDTH,
+            ),
+            Expanded(
+              child: Scaffold(
+                key: _scaffoldKey,
+                appBar: widget._appBarBuilder != null
+                    ? _appBarBuilder(context, selectedIndex)
+                    : AppBar(),
+                body: _builder(context, selectedIndex),
+              ),
+            ),
+          ]),
+          Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 8,
+                    color: Theme.of(context).colorScheme.primary,
+                  )
+                ],
+              ),
+              width: ResponsiveSize.MASTER_PANEL_WIDTH,
+              child: _buildDrawer(context, false))
+        ],
+      ),
+    );
+  }
+}
